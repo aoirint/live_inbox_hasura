44554396e2cc94044e56492f3b5431a5bc507706
aoirint <aoirint@gmail.com>
2023-12-10 17:19:33 +0900

    create table niconico_accounts

---
+++ hasura/migrations/default/1702196340336_create_table_public_niconico_accounts/up.sql
@@ -0,0 +1,18 @@
+CREATE TABLE "public"."niconico_accounts" ("id" uuid NOT NULL DEFAULT gen_random_uuid(), "created_at" timestamptz NOT NULL DEFAULT now(), "updated_at" timestamptz NOT NULL DEFAULT now(), "remote_niconico_account_id" text NOT NULL, "name" text NOT NULL, "enabled" boolean NOT NULL, PRIMARY KEY ("id") , UNIQUE ("remote_niconico_account_id"));
+CREATE OR REPLACE FUNCTION "public"."set_current_timestamp_updated_at"()
+RETURNS TRIGGER AS $$
+DECLARE
+  _new record;
+BEGIN
+  _new := NEW;
+  _new."updated_at" = NOW();
+  RETURN _new;
+END;
+$$ LANGUAGE plpgsql;
+CREATE TRIGGER "set_public_niconico_accounts_updated_at"
+BEFORE UPDATE ON "public"."niconico_accounts"
+FOR EACH ROW
+EXECUTE PROCEDURE "public"."set_current_timestamp_updated_at"();
+COMMENT ON TRIGGER "set_public_niconico_accounts_updated_at" ON "public"."niconico_accounts"
+IS 'trigger to set value of column "updated_at" to current timestamp on row update';
+CREATE EXTENSION IF NOT EXISTS pgcrypto;
