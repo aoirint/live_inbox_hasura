ea479a9fef33b4767d5f8def5e33fe683abb3fdb
aoirint <aoirint@gmail.com>
2023-12-15 17:07:31 +0900

    rename to response_text

---
+++ hasura/migrations/default/1702627638654_alter_table_public_niconico_user_broadcast_history_responses_alter_column_user_broadcast_history_response_string/down.sql
@@ -0,0 +1,1 @@
+alter table "public"."niconico_user_broadcast_history_responses" rename column "response_string" to "user_broadcast_history_response_string";
---
+++ hasura/migrations/default/1702627647276_alter_table_public_niconico_user_broadcast_history_responses_alter_column_response_string/up.sql
@@ -0,0 +1,1 @@
+alter table "public"."niconico_user_broadcast_history_responses" rename column "response_string" to "response_text";
