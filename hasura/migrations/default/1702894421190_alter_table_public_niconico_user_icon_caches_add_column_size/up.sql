b67a4e23387e413ad1d6fa3aec3eb36ac3b0920f
aoirint <aoirint@gmail.com>
2023-12-18 19:14:17 +0900

    add column niconico_user_icon_caches.size, hash_md5

---
+++ hasura/migrations/default/1702894421190_alter_table_public_niconico_user_icon_caches_add_column_size/up.sql
@@ -0,0 +1,2 @@
+alter table "public"."niconico_user_icon_caches" add column "size" integer
+ not null;
---
+++ hasura/migrations/default/1702894441913_alter_table_public_niconico_user_icon_caches_add_column_hash_md5/up.sql
@@ -0,0 +1,2 @@
+alter table "public"."niconico_user_icon_caches" add column "hash_md5" text
+ not null;
